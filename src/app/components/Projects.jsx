562fae014268e1b5e1ffdc9603efcb907b5c6572
Sohan Prabhath <soizsinghe@gmail.com>
2023-11-29 00:16:52 +0530

    projects 80% done

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,7 +1,45 @@
+'use client'
+import Image from 'next/image';
 import React from 'react'
+import bw from "../../../public/bloggerworld.png"
+import stationary from "../../../public/stationary.png"
+
 //import vid from "../../../public/vid.mp4"
 
 const Projects = () => {
+     
+    const handleButtonClick = (myproject) => {
+        window.open(myproject.url, '_blank');
+      };
+
+    const myprojects = [
+        {
+          "name": "Social Media Website - (Blogger World)",
+          "url": "https://github.com/soiz11/social-media-application.git",
+          "description":"A blogger website using MERN stack with redux. The backend is completed with a solid foundation. Currently,focused on crafting an inatative frontend and connecting it to the backend",
+          "vlink":"",
+          "imgurl":bw
+        },
+        {
+            "name": "Smart Try On App (PIKDY)",
+            "url": "https://github.com/soiz11/smart-fiton.git",
+            "description":"Smart Try On is the final year group project that involves creating a multi vendor app using Flutter,Firebase and Python. The app include a unique smart try on feature using augmented reality (AR)",
+            "vlink":"",
+            "imgurl":stationary
+        },
+        {
+            "name": "ITUM Stationary Management System",
+            "url": "https://github.com/soiz11/StationaryMS.git",
+            "description": "ITUM Stationary Management System, a group project for the Software Project Management module in the 3rd semester CA components, incorporates essential CRUD operations and authentication. Utilizing React for the frontend and Firebase for the database, it offers an efficient solution for stationary management with a user-friendly interface.",
+            "vlink": "",
+            "imgurl": stationary
+        },
+          
+        
+     
+        
+      ]
+
   return (
     <>
        <div className="w-[80vw]">
@@ -10,20 +48,26 @@ const Projects = () => {
             <div className="flex flex-1 bg-[#939393] h-[3px]"></div>
         </div>
         </div>
-
-        <div className='flex flex-col h-[fit-content]  bg-[#ededed] border-4 shadow-lg rounded-lg border-[white] w-[80vw] overflow-hidden pb-2 group '>
-        <div className=' h-[200px] xs:h-[250px] md:h-[300px] xl:h-[350px] w-[100%] relative rounded hidden group-hover:flex transition-duration-600  opacity-0 group-hover:opacity-100 transition-opacity duration-[5s]'>
-            <div className='absolute bottom-8 right-10 px-8 py-2 bg-[black] text-[white] border-[3px] text-[18px] border-[white]'>button</div>
-            <video src={require('../../../public/vid.mp4')}  width="100%" height="100%" loop muted autoPlay className='object-cover brightness-55 p-2 shadow-lg'/>
-        </div>
-            <div className='flex mt-2'>
-                <div className='hidden'>ph</div>
-                <div className='flex flex-col px-[10px]'>
-                    <div className='font-[600] text-[20px] xs:text-[22px] xl:text-[24px] md:text-[26px]'>Title</div>
-                    <div className='text-[10px] xs:text-[12px] md:text-[16px] xl:text-[18px] text-[#5a5f61]'>desvdcgvvvvvvvvvvvvvvvvvvvvv dgvfdgdg</div>
-                </div>
+        
+        { myprojects.map((myproject,index)=>(
+            <div key={index} className='flex flex-col h-[fit-content]  bg-[#ededed] border-4 shadow-lg rounded-lg border-[white] w-[80vw] overflow-hidden pb-2 group mb-9 '>
+            <div className=' h-[200px] xs:h-[250px]  w-[100%] relative rounded flex md:h-[0] md:group-hover:h-[300px] xl:group-hover:h-[350px] duration-1000 '>
+                <div  onClick={() => handleButtonClick(myproject)} className=' rounded absolute bottom-2 opacity-0 group-hover:opacity-100 group-hover:bottom-6 duration-1000  right-6 md:right-10 px-8 py-2 bg-[black] text-[white] border-[3px] text-[18px] border-[white] cursor-pointer' >Dive to the Github</div>
+                <video src={require('../../../public/vid.mp4')}  width="100%" height="100%" loop muted autoPlay className='object-cover brightness-55 p-2 group-hover:shadow-lg'/>
             </div>
-        </div> 
+                <div className='flex mt-2'>
+                    <div className='hidden md:w-[450px] md:flex md:px-2 md:group-hover:w-[0] duration-1000'>
+                        <Image src={myproject.imgurl} className='w-[100%] h-[100%] object-cover'/>
+                    </div>
+                    <div className='flex flex-col px-[10px]'>
+                        <div className='font-[500] py-3 text-[20px] xs:text-[22px] xl:text-[24px] md:text-[26px]'>{myproject.name}</div>
+                        <div className='text-[10px] pb-3 xs:text-[12px] md:text-[16px] xl:text-[18px] text-[#5a5f61]'>{myproject.description}</div>
+                    </div>
+                </div>
+            </div> 
+        ))
+        
+        }
      </>
   )
 }
