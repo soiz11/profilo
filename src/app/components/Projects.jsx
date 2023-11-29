15bb2b187130240dec523cc999522db88dc4049e
Sohan Prabhath <soizsinghe@gmail.com>
2023-11-29 09:31:59 +0530

    projects 100% done

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -34,8 +34,7 @@ const Projects = () => {
             "vlink": "",
             "imgurl": stationary
         },
-          
-        
+       
      
         
       ]
@@ -52,16 +51,16 @@ const Projects = () => {
         { myprojects.map((myproject,index)=>(
             <div key={index} className='flex flex-col h-[fit-content]  bg-[#ededed] border-4 shadow-lg rounded-lg border-[white] w-[80vw] overflow-hidden pb-2 group mb-9 '>
             <div className=' h-[200px] xs:h-[250px]  w-[100%] relative rounded flex md:h-[0] md:group-hover:h-[300px] xl:group-hover:h-[350px] duration-1000 '>
-                <div  onClick={() => handleButtonClick(myproject)} className=' rounded absolute bottom-2 opacity-0 group-hover:opacity-100 group-hover:bottom-6 duration-1000  right-6 md:right-10 px-8 py-2 bg-[black] text-[white] border-[3px] text-[18px] border-[white] cursor-pointer' >Dive to the Github</div>
+                <div  onClick={() => handleButtonClick(myproject)} className=' rounded absolute z-10 bottom-2 opacity-0 group-hover:opacity-100 group-hover:bottom-6 duration-1000  right-6 md:right-10 px-8 py-2 bg-[black] text-[white] border-[3px] text-[18px] border-[white] cursor-pointer' >Dive to the Github</div>
                 <video src={require('../../../public/vid.mp4')}  width="100%" height="100%" loop muted autoPlay className='object-cover brightness-55 p-2 group-hover:shadow-lg'/>
             </div>
                 <div className='flex mt-2'>
-                    <div className='hidden md:w-[450px] md:flex md:px-2 md:group-hover:w-[0] duration-1000'>
+                    <div className='hidden md:w-[15%] md:flex md:px-2 md:group-hover:w-[0] duration-1000'>
                         <Image src={myproject.imgurl} className='w-[100%] h-[100%] object-cover'/>
                     </div>
-                    <div className='flex flex-col px-[10px]'>
+                    <div className='flex flex-col px-[10px] w-[85%]'>
                         <div className='font-[500] py-3 text-[20px] xs:text-[22px] xl:text-[24px] md:text-[26px]'>{myproject.name}</div>
-                        <div className='text-[10px] pb-3 xs:text-[12px] md:text-[16px] xl:text-[18px] text-[#5a5f61]'>{myproject.description}</div>
+                        <div className='text-[10px] pb-3 xs:text-[12px] md:text-[16px] xl:text-[18px] text-[#5a5f61] flex'>{myproject.description}</div>
                     </div>
                 </div>
             </div> 
