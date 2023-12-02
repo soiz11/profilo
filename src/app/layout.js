1318c4bbd6d55b463f3450891d9c436102042e1a
Sohan Prabhath <soizsinghe@gmail.com>
2023-12-02 15:41:49 +0530

    all done

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const poppins = Poppins({ subsets: ['latin'], weight:["100","200","300","400","500","600"] })
 
 export const metadata = {
-  title: 'Sohan.One',
+  title: 'sohan.One',
   description: 'Profilo with NextJs',
 }
 
